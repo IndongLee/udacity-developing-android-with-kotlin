da15e340ed387852fbf37c4e0ff2195f49ae1644
IndongLee <kundera@kakao.com>
2020-06-06 23:50:54 +0900

    [Add] Safe Arguments

--- 04_Trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ 04_Trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -33,9 +34,18 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
 
+//        binding.nextMatchButton.setOnClickListener { view: View ->
+//            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+//        }
+
         binding.nextMatchButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
+
+        var args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context,
+                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+                Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
--- 04_Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ 04_Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -27,9 +27,13 @@ class TitleFragment : Fragment() {
 //            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
 //        }
 
-        binding.playButton.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        )
+//        binding.playButton.setOnClickListener(
+//                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+//        )
+
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
         setHasOptionsMenu(true)
         return binding.root
     }
