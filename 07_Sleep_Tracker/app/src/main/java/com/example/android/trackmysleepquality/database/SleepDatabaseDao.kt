8d8cd47fa22d9d2710bf6b95089e4cd8d9d78a66
IndongLee <kundera@kakao.com>
2020-06-20 11:40:16 +0900

    [Add] DAO

--- 07_Sleep_Tracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ 07_Sleep_Tracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,30 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao {
+
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight
+
+//    @Delete
+//    fun deleteAllNight(nights: List<SleepNight>): Int
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId LIMIT 1")
+    fun getTonight(): SleepNight?
+}
+
